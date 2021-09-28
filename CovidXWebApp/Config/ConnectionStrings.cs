a839da09aee91dba746b219c736cf5d0bb505b43
Lubambo <lubambolorenzo10.nkayi@gmail.com>
2021-09-28 21:55:07 +0200

    Configuring the StartUp nd also the Connection string

---
+++ CovidXWebApp/Config/ConnectionStrings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CovidXWebApp.Config
+{
+    public class ConnectionStrings : IConnectionStrings
+    {
+        public string DefaultConnection { get; set; }
+    }
+}
